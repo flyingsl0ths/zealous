a4e93aa3b52294b6b3f5b0e0baef7135494ef1b2
flyingsl0ths <randomemail33110@protonmail.com>
2024-07-28 08:16:27 -0700

    Implement literal parsing

--- src/parser.zig
+++ src/parser.zig
@@ -1,4 +1,44 @@
+const std = @import("std");
+
 const lexer = @import("lexer.zig");
 const object = @import("object.zig");
+const ParserResult = union(enum) {
+    err: lexer.TokenError,
+    value: object.Value,
+};
 
-pub fn parse(_: lexer.str) error{}!object.Value {}
+pub fn parse(source: lexer.str) ParserResult {
+    const lexr = lexer.Lexer.init(source);
+    switch (lexer.scan(&lexr)) {
+        .tokenError => |err| {
+            return ParserResult{ .err = err };
+        },
+        .token => |tk| {
+            switch (tk.type_) {
+                .Int => {
+                    const str = source[tk.start..tk.length];
+                    const num = std.fmt.parseInt(i32, str, 10);
+                    return object.Value{ .number = object.Number{ .integer = num } };
+                },
+                .Float => {
+                    const str = source[tk.start..tk.length];
+                    const num = std.fmt.parseFloat(f64, str);
+                    return object.Value{ .number = object.Number{ .float = num } };
+                },
+                .String => {
+                    const str = source[tk.start..tk.length];
+                    return object.Value{ .string = str };
+                },
+                .True => {
+                    return object.Value{ .boolean = true };
+                },
+                .False => {
+                    return object.Value{ .boolean = false };
+                },
+                .Null => {
+                    return object.Value{ .null_ = null };
+                },
+            }
+        },
+    }
+}
